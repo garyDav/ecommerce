35a69b07c472eeb392b7e1ef5aa66bf4d5a7a8cc
garyDav <gary.2810.dav@gmail.com>
2020-07-24 09:32:34 -0400

    parcial terminado

--- utils/mocks/parcial.js
+++ utils/mocks/parcial.js
@@ -1,4 +1,8 @@
 function combinatoria(x, n) {
+  if(x>n)
+    return 'error: x debe ser menor que n'
+  if(x<0)
+    return 'error: x no debe ser negativo'
   return factorial(n)/(factorial(x)*factorial(n-x))
 }
 
